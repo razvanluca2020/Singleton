3fc2acf353ad0d2bf0d21062beb78288e7341c5c
Razvan Luca <raz1ro@yahoo.com>
2021-08-17 18:43:15 +0300

    Added singlenton logger implementation

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Logger.getInstance();
+        Logger.getInstance().warning("This is a warning log");
+        Logger.getInstance().info("This is a info log");
+    }
+}
